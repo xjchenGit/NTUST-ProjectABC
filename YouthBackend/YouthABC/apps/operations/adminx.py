daa4c2d1ea768582cbfd86e63c93e15e2495258f
VictorChan1 <achenxuanjun@gmail.com>
2019-07-29 13:56:54 +0800

    update requirements.txt

--- YouthBackend/YouthABC/apps/operations/adminx.py
+++ YouthBackend/YouthABC/apps/operations/adminx.py
@@ -2,17 +2,27 @@ import xadmin
 from xadmin import views
 
 from .models import Banner,UserAsk,CourseComments
-from .models import UserFavorite,UserMessage
+from .models import UserFavorite,UserMessage,UserCourse
 
 
 # Register your models here.
 
-class CoursesAdmin(object):
-    list_display = ['name', 'detail']
+class BannerAdmin(object):
+    list_display = ['title', 'image','url','index']
+    search_fields = ['title']
+    model_icon = 'fa fa-telegram'
+
+class UserAskAdmin(object):
+    list_display = ['name','mobile','course_name']
     search_fields = ['name']
     model_icon = 'fa fa-telegram'
-    style_fields = {"detail":"ueditor"}
 
+class CourseCommentsAdmin(object):
+    list_display = ['user','course','comments']
+    search_fields = ['user','course','comments']
+    model_icon = 'fa fa-telegram'
 
-xadmin.site.register(Course, CoursesAdmin)
+xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(UserAsk, UserAskAdmin)
+xadmin.site.register(CourseComments, CourseCommentsAdmin)
 # Register your models here.
--- YouthBackend/YouthABC/apps/users/adminx.py
+++ YouthBackend/YouthABC/apps/users/adminx.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
+import xadmin
+from xadmin import views
 
+from .models import UserProfile,ChildrenProfile
 # Register your models here.
+
+
+class UserProfileAdmin(object):
+    list_display = ['nick_name', 'city_name', 'add_time']
+    search_fields = ['province_name', 'city_name']
+    model_icon = 'fa fa-telegram'
+
+xadmin.site.register(CityDict, CityDictAdmin)
